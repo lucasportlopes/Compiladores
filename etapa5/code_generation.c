6f10cb2cc35eea3b11b695a09030078ab82cfe05
Lucas Lopes <lopeslucas855@gmail.com>
2024-12-07 15:44:34 -0300

    etapa5

--- etapa5/code_generation.c
+++ etapa5/code_generation.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int tmp_counter = 0;
-char *generate_temp();
+static int tmp_counter = 0;
+static int label_counter = 0;
+
 char *generate_temp() {
-  char *tmp = malloc(6);
+  char *tmp = malloc(16 * sizeof(char));
   sprintf(tmp, "r%d", tmp_counter++);
   return tmp;
+}
+
+char *generate_label() {
+  char *label = malloc(16 * sizeof(char));
+  sprintf(label, "L%d", label_counter++);
+  return label;
 }
--- etapa5/code_generation.h
+++ etapa5/code_generation.h
@@ -1,6 +1,21 @@
 #ifndef CODE_GENERATION_H
 #define CODE_GENERATION_H
 
+typedef struct {
+    char *opcode;
+    char *source1;
+    char *source2;
+    char *source3;
+    char *label;
+} ILOCOperation;
+
+typedef struct ILOCOperationNode {
+    ILOCOperation *operation;
+    struct ILOCOperationNode *next;
+} ILOCOperationNode;
+
+char *generate_label();
+
 char *generate_temp();
 
 #endif
